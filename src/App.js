8676ee1757b36822e3dc8f77f766a091597eed0a
ms.moradi <ms.moradi28@gmail.com>
2023-05-16 20:03:46 +0430

    add themeProvider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme/theme";
 
 function App() {
   return (
-    <>
-    </>
+    <ThemeProvider theme={theme}>
+      
+    </ThemeProvider>
   );
 }
 
