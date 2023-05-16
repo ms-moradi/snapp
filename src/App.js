d621180301cb9672c90a13066eb76f708a02bd3a
ms.moradi <ms.moradi28@gmail.com>
2023-05-16 20:08:27 +0430

    add routerProvider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme/theme";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router";
+
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      
+      <RouterProvider router={router}/>
     </ThemeProvider>
   );
 }
