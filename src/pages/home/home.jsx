b6355ff372009ff5c095c100806bc07e1d6202f9
ms.moradi <ms.moradi28@gmail.com>
2023-05-18 18:59:05 +0430

    add components

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,16 @@
 import Header from '../../components/header/header';
+import Services from '../../components/services/services';
+import ServiceMenu from '../../components/serviceMenu/servicesMenu';
 
 const Home =()=>{
     return(
         <>
             <Header/>
+            <main style={{paddingX:'20px'}}>
+                <Services/>
+                <ServiceMenu/>
+            </main>
+            
         </>
     )
 }
