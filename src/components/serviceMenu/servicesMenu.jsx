6a66354708aefe078657e29d36dbe8570be0bf74
ms.moradi <ms.moradi28@gmail.com>
2023-05-19 20:02:14 +0430

    add zIndex style, transfer the bgColor to the inner Grid

--- src/components/serviceMenu/servicesMenu.jsx
+++ src/components/serviceMenu/servicesMenu.jsx
@@ -8,8 +8,8 @@ import Grid from "@mui/material/Grid";
 
 const ServiceMenu =()=>{
     return(
-        <Grid width={'100%'} backgroundColor={'white'} display={'flex'} justifyContent={'center'} position={'fixed'} bottom={'20px'}>
-            <Grid width={'85%'} display={'flex'} px={1} borderRadius={'15px'} boxShadow={'0px 5px 10px 0px #e0e0e0'}> 
+        <Grid width={'100%'} display={'flex'} justifyContent={'center'} position={'fixed'} bottom={'20px'} zIndex={'10'}>
+            <Grid width={'85%'} backgroundColor={'white'} display={'flex'} px={1} borderRadius={'15px'} boxShadow={'0px 5px 10px 0px #e0e0e0'}> 
                 {ServiceMenuHome.map((item)=>(
                     <ServiceIcon key={item.id} src={item.icon} title={item.title}/>
                 ))}
