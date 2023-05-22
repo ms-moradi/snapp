b2743163be349f6f424351ba71eafe544aedcb4c
ms.moradi <ms.moradi28@gmail.com>
2023-05-22 17:37:54 +0430

    create ,add style and import swiper

---
+++ src/components/containerSwiper/containerSwiper.jsx
@@ -0,0 +1,20 @@
+
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import SquareIcon from '@mui/icons-material/Square';
+
+import MainSwiper from '../mainSwiper/mainSwiper';
+
+const ContainerSwiper =({header, database, titleBtn, bgColor})=>{
+    return(
+        <Grid>
+            <Grid display={'flex'} justifyContent={'flex-start'} alignItems={'center'} gap={'5px'} my={1}>
+                <SquareIcon sx={{color: bgColor, fontSize: 10}}/>
+                <Typography fontSize={'16px'}>{header}</Typography>
+            </Grid>
+            <MainSwiper database={database} titleBtn={titleBtn}/>
+        </Grid>
+    )
+}
+
+export default ContainerSwiper;
--- src/components/serviceMenu/servicesMenu.jsx
+++ src/components/serviceMenu/servicesMenu.jsx
@@ -9,7 +9,7 @@ import Grid from "@mui/material/Grid";
 const ServiceMenu =()=>{
     return(
         <Grid width={'100%'} display={'flex'} justifyContent={'center'} position={'fixed'} bottom={'20px'} zIndex={'10'}>
-            <Grid xs={{width:'350px'}} backgroundColor={'white'} display={'flex'} px={1} borderRadius={'15px'} boxShadow={'0px 5px 10px 0px #e0e0e0'}> 
+            <Grid width={'280px'} backgroundColor={'white'} display={'flex'} px={1} borderRadius={'15px'} boxShadow={'0px 5px 10px 0px #e0e0e0'}> 
                 {ServiceMenuHome.map((item)=>(
                     <ServiceIcon key={item.id} src={item.icon} title={item.title}/>
                 ))}
