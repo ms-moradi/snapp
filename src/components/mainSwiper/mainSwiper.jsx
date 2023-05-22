c6d7cd3c2f0f0913cbe0937ea837fab1bfec15e2
ms.moradi <ms.moradi28@gmail.com>
2023-05-22 17:43:37 +0430

    create and style and import card component

---
+++ src/components/mainSwiper/mainSwiper.jsx
@@ -0,0 +1,44 @@
+import { Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import "swiper/css";
+
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+import Card from '../card/card';
+
+const MainSwiper =({database, titleBtn})=>{
+    return (
+        <Grid>
+            <Swiper 
+                style={{
+                    width:'100%',
+                    height: '170px',
+                    borderRadius:'10px',
+                }}
+                loop={true}
+                effect={"coverflow"}
+                spaceBetween={'10px'}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={"auto"}
+                modules={[Navigation]}
+            >   
+                <Grid height={'150px'} >
+                    <Typography></Typography>
+                    <Grid  boxShadow={'0px 5px 10px 0 grey'}>
+                        {database.map((item)=>(
+                            <SwiperSlide>
+                               <Card id={item.id} image={item.src} icon={item.icon} title={item.title} titleBtn={titleBtn}/>
+                            </SwiperSlide>
+                        ))}
+                    </Grid>
+                </Grid>
+            </Swiper>
+        </Grid>
+    )
+}
+
+export default MainSwiper;
