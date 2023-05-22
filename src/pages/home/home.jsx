726f073870d0fa8a1778c7c14180f3c0e5a76087
ms.moradi <ms.moradi28@gmail.com>
2023-05-22 17:44:19 +0430

    import database and add containerSwiper

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,6 +2,17 @@ import Header from '../../components/header/header';
 import Services from '../../components/services/services';
 import ServiceMenu from '../../components/serviceMenu/servicesMenu';
 import AdvertiseSwiper from '../../components/swiper/swiper';
+import ContainerSwiper from '../../components/containerSwiper/containerSwiper';
+
+import { Food } from '../../database/dataSwiper';
+import { Trip } from '../../database/dataSwiper';
+import { Discount } from '../../database/dataSwiper';
+
+
+import iconFood from '../../assets/image/icon/Food.png';
+import iconTrip from '../../assets/image/icon/Hotel.png';
+
+
 
 
 const Home =()=>{
@@ -12,6 +23,9 @@ const Home =()=>{
                 <Services/>
                 <ServiceMenu/>
                 <AdvertiseSwiper/>
+                <ContainerSwiper header={'انتخاب از بین غذاهای متنوع'} database={Food} titleBtn={'سفارش دهید'} bgColor={'#ff00a4'}/>
+                <ContainerSwiper header={'مسافرت راحت با اسنپ'} database={Trip} titleBtn={'بیشتر بدانید'} bgColor={'#ff4340'}/>
+                <ContainerSwiper header={'خرید از فروشگاه با تخفیف بهاره'} database={Discount} titleBtn={'خرید کنید'} bgColor={'#c300b4'}/>
             </main>
             
         </>
