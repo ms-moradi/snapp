c9e1bc3de44cccf1db65ca43640891e599e0536e
ms.moradi <ms.moradi28@gmail.com>
2023-05-23 14:32:00 +0430

    create and style

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -2,18 +2,29 @@
 
 import Grid from '@mui/material/Grid';
 import Typography  from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import SquareIcon from '@mui/icons-material/Square';
 
-import Card from '../card/card';
 
-const Banner =({bgColor, header, id , src , icon , title, titleBtn})=>{
+const Banner =({bgColor, header, image , icon , title, titleBtn})=>{
     return(
-        <Grid>
-            <Grid>
+        <Grid width={'100%'} height={'317px'} borderRadius={'10px'} display={'flex'} flexDirection={'column'} alignItems={'flex-start'} justifyContent={'center'}>
+            <Grid width={'100%'} height={'45px'} display={'flex'} justifyContent={'flex-start'} alignItems={'center'} gap={'10px'}>
                 <SquareIcon sx={{color: bgColor, fontSize: 10}}/>
-                <Typography>{header}</Typography>
+                <Typography fontSize={'12px'}>{header}</Typography>
             </Grid>
-            <Card id={id} image={src} icon={icon} title={title} titleBtn={titleBtn}/>
+            <Grid width={'100%'} height={'244px'}  boxShadow={'0px 8px 5px 0px #e9e9e9'} position={'relative'} display={'flex'} flexDirection={'column'} alignContent={'center'} justifyContent={'center'}>
+                <img src={image} alt='banner' style={{width:'100%', height:'224px', borderRadius:'10px' }}/>  
+                <Grid width={'100%'} height={'48px'} display={'flex'} justifyContent={'space-between'} alignContent={'baseline'} borderRadius={'0 0 10px 10px'} position={'absolute'} backgroundColor={'white'} bottom={'0px'} p={'8px'} >
+                    <Grid display={'flex'} justifyContent={'flex-start'} alignItems={'center'} gap={'10px'} >
+                        <img src={icon} alt='icon' style={{width:'32px', height:'32px'}}/>
+                        <Typography fontSize={'12px'}>{title}</Typography>
+                    </Grid>
+                    <Grid>
+                        <Button variant="outlined" color={'success'} style={{fontSize:'12px'}}>{titleBtn}</Button>
+                    </Grid>
+                </Grid>
+            </Grid>    
         </Grid>
     )
 }
