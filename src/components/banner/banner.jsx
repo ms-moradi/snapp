fda74b7cfff1f4aef06b7cee99987e569f5780cd
ms.moradi <ms.moradi28@gmail.com>
2023-05-23 12:03:39 +0430

    create banner components

---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,20 @@
+
+
+import Grid from '@mui/material/Grid';
+import SquareIcon from '@mui/icons-material/Square';
+
+import Card from '../card/card';
+
+const Banner =({bgColor, header, id , src , icon , title, titleBtn})=>{
+    return(
+        <Grid>
+            <Grid>
+                <SquareIcon sx={{color: bgColor, fontSize: 10}}/>
+                <Typography>{header}</Typography>
+            </Grid>
+            <Card id={id} image={src} icon={icon} title={title} titleBtn={titleBtn}/>
+        </Grid>
+    )
+}
+
+export default Banner;
