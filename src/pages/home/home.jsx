df2d8e5bedd8ed2565d7066b385aad05070a1acb
ms.moradi <ms.moradi28@gmail.com>
2023-05-23 12:03:17 +0430

    import database and add container swiper

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -7,12 +7,10 @@ import ContainerSwiper from '../../components/containerSwiper/containerSwiper';
 import { Food } from '../../database/dataSwiper';
 import { Trip } from '../../database/dataSwiper';
 import { Discount } from '../../database/dataSwiper';
-
-
-import iconFood from '../../assets/image/icon/Food.png';
-import iconTrip from '../../assets/image/icon/Hotel.png';
-
-
+import { Doctor } from '../../database/dataSwiper';
+import { Hotel } from '../../database/dataSwiper';
+import { Market } from '../../database/dataSwiper';
+import { Carousel } from '../../database/dataSwiper';
 
 
 const Home =()=>{
@@ -26,6 +24,11 @@ const Home =()=>{
                 <ContainerSwiper header={'انتخاب از بین غذاهای متنوع'} database={Food} titleBtn={'سفارش دهید'} bgColor={'#ff00a4'}/>
                 <ContainerSwiper header={'مسافرت راحت با اسنپ'} database={Trip} titleBtn={'بیشتر بدانید'} bgColor={'#ff4340'}/>
                 <ContainerSwiper header={'خرید از فروشگاه با تخفیف بهاره'} database={Discount} titleBtn={'خرید کنید'} bgColor={'#c300b4'}/>
+                <ContainerSwiper header={'خرید در ۴ قسط با تخفیف'} database={Carousel} titleBtn={'خرید کنید'} bgColor={'#58C0F9'}/>
+                <ContainerSwiper header={'زیر ۳۰ دقیقه تحویل بگیرید'} database={Market} titleBtn={'خرید کنید'} bgColor={'#FF7300'}/>
+                <ContainerSwiper header={'با بهترین قیمت پرواز خارجی بخرید'} database={Trip} titleBtn={'بلیت بخرید'} bgColor={'#ff4340'}/>
+                <ContainerSwiper header={'با بهترین قیمت هتل خارجی رزرو کنید'} database={Hotel} titleBtn={'بلیت بخرید'} bgColor={'#ff4340'}/>
+                <ContainerSwiper header={'انلاین, خدمات پزشکی بگیرید'} database={Doctor} titleBtn={'درخواست دهید'} bgColor={'#58C0F9'}/>
             </main>
             
         </>
