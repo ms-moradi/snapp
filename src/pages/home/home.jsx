b0c5d6d406f3eaf1ec1bc7d34163960320e0d1cf
ms.moradi <ms.moradi28@gmail.com>
2023-05-23 14:32:41 +0430

    import banner component

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,6 +3,7 @@ import Services from '../../components/services/services';
 import ServiceMenu from '../../components/serviceMenu/servicesMenu';
 import AdvertiseSwiper from '../../components/swiper/swiper';
 import ContainerSwiper from '../../components/containerSwiper/containerSwiper';
+import Banner from '../../components/banner/banner';
 
 import { Food } from '../../database/dataSwiper';
 import { Trip } from '../../database/dataSwiper';
@@ -12,17 +13,24 @@ import { Hotel } from '../../database/dataSwiper';
 import { Market } from '../../database/dataSwiper';
 import { Carousel } from '../../database/dataSwiper';
 
+import driver from '../../assets/image/icon/Driver.png';
+import bus from '../../assets/image/icon/snapptrip-bus.png';
+
+import bannerDriver from '../../assets/image/banner/bannerDriver.jpg';
+import bannerBus from '../../assets/image/banner/bannerBus.jpg';
 
 const Home =()=>{
     return(
         <>
             <Header/>
-            <main style={{paddingX:'20px',alignItems:'center'}}>
+            <main style={{paddingX:'40px',alignItems:'center'}}>
                 <Services/>
                 <ServiceMenu/>
                 <AdvertiseSwiper/>
                 <ContainerSwiper header={'انتخاب از بین غذاهای متنوع'} database={Food} titleBtn={'سفارش دهید'} bgColor={'#ff00a4'}/>
                 <ContainerSwiper header={'مسافرت راحت با اسنپ'} database={Trip} titleBtn={'بیشتر بدانید'} bgColor={'#ff4340'}/>
+                <Banner bgColor={'#00D170'} header={'رانندگی با درآمد بالا'} image={bannerDriver} icon={driver} title={'تا ۱۸ میلیون درآمد ماهیانه'} titleBtn={'ثبت نام کنید'}/>
+                <Banner bgColor={'#ff4340'} header={'خرید بلیت قطار با تخفیف'} image={bannerBus} icon={bus} title={'ویژه ی اولین خرید'} titleBtn={'بلیت بخرید'}/>
                 <ContainerSwiper header={'خرید از فروشگاه با تخفیف بهاره'} database={Discount} titleBtn={'خرید کنید'} bgColor={'#c300b4'}/>
                 <ContainerSwiper header={'خرید در ۴ قسط با تخفیف'} database={Carousel} titleBtn={'خرید کنید'} bgColor={'#58C0F9'}/>
                 <ContainerSwiper header={'زیر ۳۰ دقیقه تحویل بگیرید'} database={Market} titleBtn={'خرید کنید'} bgColor={'#FF7300'}/>
