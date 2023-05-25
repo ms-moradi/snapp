9f29c596f42e4d05f52415a062282f49e909e5f6
ms.moradi <ms.moradi28@gmail.com>
2023-05-25 18:25:51 +0430

    modify style swiper and slides swiper

--- src/components/mainSwiper/mainSwiper.jsx
+++ src/components/mainSwiper/mainSwiper.jsx
@@ -1,4 +1,4 @@
-import { Navigation } from "swiper";
+import { Navigation , Pagination} from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.min.css";
 import "swiper/swiper.min.css";
@@ -18,19 +18,30 @@ const MainSwiper =({database, titleBtn})=>{
                     height: '150px',
                     borderRadius:'10px',
                 }}
-                loop={true}
-                effect={"coverflow"}
-                spaceBetween={'10px'}
-                grabCursor={true}
-                centeredSlides={true}
-                slidesPerView={"1"}
-                modules={[Navigation]}
+                slidesPerView={'auto'}
+                freeMode={true}
+                pagination= {{
+                    el: '.swiper-pagination',
+                    clickable: true,
+                }}
+                breakpoints={{
+                    0:{
+                        spaceBetween:'10'
+                    },
+                    768:{
+                        spaceBetween:'20'
+                    },
+                    1024:{
+                        spaceBetween:'30',
+                    }
+                }}
+                modules={[Navigation, Pagination]}
             >   
                 <Grid height={'150px'} >
                     <Typography></Typography>
-                    <Grid  boxShadow={'0px 5px 10px 0 grey'}>
+                    <Grid  boxShadow={'0px 5px 10px 0 grey'} display={'flex'} justifyContent={'center'} mr={2}>
                         {database.map((item)=>(
-                            <SwiperSlide style={{marginBottom:'0px'}}>
+                            <SwiperSlide style={{marginBottom:'0px', width:'300px'}}>
                                <Card id={item.id} image={item.src} icon={item.icon} title={item.title} titleBtn={titleBtn}/>
                             </SwiperSlide>
                         ))}
