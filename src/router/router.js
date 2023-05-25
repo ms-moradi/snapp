d92d41881a5c96f424d9949939cc299a761165e5
ms.moradi <ms.moradi28@gmail.com>
2023-05-25 21:19:08 +0430

    add Layout and import account, payment, club, discount page

--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,39 @@
 import { createBrowserRouter } from "react-router-dom";
+
+import Layout from "../components/layout/layout";
 import Home from "../pages/home/home";
+import Club from "../pages/club/club";
+import Discount from "../pages/discount/discount";
+import Payment from "../pages/payment/payment";
+import Account from "../pages/account/account";
 
 const router = createBrowserRouter([
     {
-        path: "/",
-        element: <Home/>,
+        path:"/",
+        element: <Layout/>,
+        children:[
+            {
+                index:true,
+                element:<Home/>
+            },
+            {
+                path:"/club",
+                element:<Club/>
+            },
+            {
+                path:"/discount",
+                element:<Discount/>
+            }
+        ]
     },
+    {
+        path:'/payment',
+        element:<Payment/>
+    },
+    {
+        path:'/account',
+        element:<Account/>
+    }
 
 ])
 
