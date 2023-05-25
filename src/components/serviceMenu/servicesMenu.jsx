e8a2270e6aaf38c1f2150e8b63b7b080b065c392
ms.moradi <ms.moradi28@gmail.com>
2023-05-25 21:17:35 +0430

    change background to white smoke

--- src/components/serviceMenu/servicesMenu.jsx
+++ src/components/serviceMenu/servicesMenu.jsx
@@ -9,7 +9,7 @@ import Grid from "@mui/material/Grid";
 const ServiceMenu =()=>{
     return(
         <Grid width={'100%'} display={'flex'} justifyContent={'center'} position={'fixed'} bottom={'20px'} zIndex={'10'}>
-            <Grid width={'280px'} backgroundColor={'white'} display={'flex'} px={1} borderRadius={'15px'} boxShadow={'0px 5px 15px 5px #757575'}> 
+            <Grid width={'280px'} backgroundColor={'white'} display={'flex'} px={1} borderRadius={'15px'} boxShadow={'0px 5px 15px 5px whiteSmoke'}> 
                 {ServiceMenuHome.map((item)=>(
                     <ServiceIcon key={item.id} src={item.icon} title={item.title}/>
                 ))}
