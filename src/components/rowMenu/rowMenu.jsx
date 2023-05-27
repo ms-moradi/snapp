ac8cd98b0c8932c673bdaf3c860860d8ec5b079b
ms.moradi <ms.moradi28@gmail.com>
2023-05-27 14:59:33 +0430

    create and add styles

---
+++ src/components/rowMenu/rowMenu.jsx
@@ -0,0 +1,19 @@
+
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+const RowMenu =({id, icon, title, element})=>{
+    return(
+        <Grid key={id} height={'50px'} display={'flex'} justifyContent={'flex-start'} alignItems={'center'} gap={'15px'}>
+            <Grid>
+                <img src={icon} alt='logo'style={{width:'20px', height:'20px'}}/>
+            </Grid>
+            <Grid width={'100%'} display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
+                <Typography>{title}</Typography>
+                {element}
+            </Grid>
+        </Grid>
+    )
+}
+
+export default RowMenu;
