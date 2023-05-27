53cb2900f6858cddd2538bbab1952fdbdfc089e3
ms.moradi <ms.moradi28@gmail.com>
2023-05-27 18:55:56 +0430

    add useState, customDialog and  props to components

--- src/pages/payment/payment.jsx
+++ src/pages/payment/payment.jsx
@@ -1,19 +1,26 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
 
 import RowMenu from '../../components/rowMenu/rowMenu';
 import {payment} from '../../database/database';
 import Inventory from '../../components/inventory/inventory';
-
+import CustomDialog from '../../components/dialog/dialog';
 
 const Payment =()=>{
+    const [open, setOpen] = useState('false')
+
     return(
         <Grid display={'flex'} flexDirection={'column'} alignContent={'space-between'} mt={8} >
             <Grid p={'0 15px'}>
                 {payment.map((item)=>(
-                    <RowMenu id={item.id} title={item.title} icon={item.src} element={item.element}/>
+                    <Link to={item?.link} style={{textDecoration:'none'}}>
+                        <RowMenu id={item.id} title={item.title} icon={item.src} element={item.element}/>
+                    </Link>
                 ))}
             </Grid>
-            <Inventory/>
+            <Inventory setOpen={setOpen}/>
+            <CustomDialog open={open} setOpen={setOpen}/>
         </Grid>
     )
 }
