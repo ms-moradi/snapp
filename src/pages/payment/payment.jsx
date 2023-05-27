ad1c79641bc6ae5121a53ee167a8fe55898ca6d5
ms.moradi <ms.moradi28@gmail.com>
2023-05-27 15:00:19 +0430

    import RowMenu and add styles

--- src/pages/payment/payment.jsx
+++ src/pages/payment/payment.jsx
@@ -1,10 +1,20 @@
+import Grid from '@mui/material/Grid';
+
+import RowMenu from '../../components/rowMenu/rowMenu';
+import {payment} from '../../database/database';
+import Inventory from '../../components/inventory/inventory';
 
 
 const Payment =()=>{
     return(
-        <>
-            payment
-        </>
+        <Grid display={'flex'} flexDirection={'column'} alignContent={'space-between'} mt={8} >
+            <Grid p={'0 15px'}>
+                {payment.map((item)=>(
+                    <RowMenu id={item.id} title={item.title} icon={item.src} element={item.element}/>
+                ))}
+            </Grid>
+            <Inventory/>
+        </Grid>
     )
 }
 
