f9b7a3f1824d24b358d83867c61b104a610c99b6
ms.moradi <ms.moradi28@gmail.com>
2023-05-29 20:25:07 +0430

    add element from material and add styles

--- src/pages/cardSnapp/cardSnapp.jsx
+++ src/pages/cardSnapp/cardSnapp.jsx
@@ -1,10 +1,37 @@
 
+import logo from '../../assets/image/logo/cardSnapp.png';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import StyleIcon from '@mui/icons-material/Style';
+import CustomButton from '../../components/button/button';
 
 const CardSnapp =()=>{
     return(
-        <>
-            
-        </>
+        <Grid mt={8} px={2}>
+           <img src={logo} alt='logo' style={{width:'300px'}}/> 
+           <Typography>کد اسنپ کارتتان را وارد کنید</Typography>
+           <Typography fontSize='14px'>
+            از موجودی اسنپ کارت می توانید برای پرداخت هزینه تمام <br/>
+           سرویس ها (ماشین , موتور و پیک) استفاده کنید
+           </Typography>
+           <Grid my={5}>
+                <TextField 
+                label="کداسنپ کارت" 
+                variant="outlined" 
+                InputProps={{
+                startAdornment: (
+                    <InputAdornment position="start">
+                        <StyleIcon />
+                    </InputAdornment>
+                ),
+                }}
+                size='small'
+                />
+           </Grid>
+           <CustomButton title={'اعمال'} variant={'disable'}/>
+        </Grid>
     )
 }
 
