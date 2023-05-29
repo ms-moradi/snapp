1ae7c910c076e03a059a8e1a493b020fa6487562
ms.moradi <ms.moradi28@gmail.com>
2023-05-29 20:27:42 +0430

    import and add Link

--- src/components/serviceIcon/serviceIcon.jsx
+++ src/components/serviceIcon/serviceIcon.jsx
@@ -1,16 +1,20 @@
+import { Link } from "react-router-dom";
+
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
-const ServiceIcon =({key, src, title})=>{
+const ServiceIcon =({key, src, title, link})=>{
     return(
-        <Grid key={key} width={'25%'} display={'flex'} flexDirection={'column'} alignItems={'center'} my={1}>
-            <Grid>
-                <img src={src} alt="service" style={{width:'40px', height:'40px'}}/>
+        <Link to={link} style={{ width:'25%', textDecoration:'none', color:'#000'}}>
+            <Grid key={key} width={'100%'} display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} alignSelf={'center'} my={1}>
+                <Grid>
+                    <img src={src} alt="service" style={{width:'40px', height:'40px'}}/>
+                </Grid>
+                <Grid>
+                    <Typography fontSize={'11px'}>{title}</Typography>
+                </Grid>
             </Grid>
-            <Grid>
-                <Typography fontSize={'12px'}>{title}</Typography>
-            </Grid>
-        </Grid>
+        </Link>
     )
 }
 
